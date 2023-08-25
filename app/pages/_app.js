64e65aa41aa81d0f7240968d359831fe0e1dd542
Mahbub Morshed <mahbub.bogra30@gmail.com>
2023-08-26 05:32:24 +0600

    PWA and tailwind install

---
+++ app/pages/_app.js
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
+const withPWAInit = require("next-pwa");
+
+/** @type {import('next-pwa').PWAConfig} */
+const withPWA = withPWAInit({
+  dest: "public",
+  // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
+  buildExcludes: ["app-build-manifest.json"],
+});
+
 /** @type {import('next').NextConfig} */
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
+
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
