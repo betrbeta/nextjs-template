34027a5098f6e8c70a395eee4eddb59405c05c26
rudenkoserhii <rudenko.sergey@ukr.net>
2023-11-22 10:12:36 +0200

    fix feedback comp

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -4,12 +4,14 @@ import { SendEmail } from "../SendEmail/SendEmail";
 export const Header = () => {
   return (
     <div className="flex justify-between items-center mt-1 transition-all">
-      <div>
+      <div className="flex justify-center items-center transition-all">
         <button className="px-6 py-3 text-white transition-all hover:bg-white hover:text-[#330033] rounded-s-[22px] border-[3px] border-black border-solid">
           <Link href="/mySubscription">Subscription</Link>
         </button>
         <button className="px-6 py-3 text-white transition-all hover:bg-white hover:text-[#330033] rounded-e-[22px] border-[3px] border-l-0 border-black border-solid">
-          <Link href="/feedback">Feedback Form</Link>
+          <Link href="/feedback" className="whitespace-nowrap">
+            Feedback Form
+          </Link>
         </button>
       </div>
       <SendEmail />
