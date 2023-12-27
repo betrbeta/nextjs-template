261298ad67c79c23506047bf99c46fe379859227
prog <email@gmail.com>
2023-12-27 19:10:02 +0700

    chore: Cypress E2E home page ✔

--- lib/logger/logger.ts
+++ lib/logger/logger.ts
@@ -1,12 +1,15 @@
 import pino from 'pino';
+// import pretty from 'pino-pretty'
 
 const logger = pino({
   browser: {},
-  level: 'debug',
+  level: process.env.NEXT_PUBLIC_PINO_LOG_LEVEL,
   transport: {
     target: 'pino-pretty',
     options: {
       colorize: true,
+      timestampKey: 'time',
+      translateTime: true, // Human-readable timestamps
     },
   },
   base: {
@@ -14,5 +17,18 @@ const logger = pino({
     revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
   },
 });
-
+/*const logger = pino({
+  level: process.env.NEXT_PUBLIC_LOG_LEVEL,
+  formatters: {
+    level: (label) => {
+      return { level: label.toUpperCase() };
+    },
+    bindings: (bindings) => {
+      return { host: bindings.hostname };
+    },
+  },
+  transport: {
+    target: 'pino-pretty',
+  },
+});*/
 export default logger;
