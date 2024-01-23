327e323ab09c6cc0994e85a98c65f18ef3e38247
prog <email@gmail.com>
2024-01-23 22:21:53 +0700

    cicd: Formatting code to trigger Actions build

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState, ChangeEvent} from "react";
+import React, {useState, ChangeEvent, useEffect} from "react";
 import "./styles/style.css";
 import CookieBanner from "./components/CookiesBanner/CookiesBanner";
 import Link from "next/link";
@@ -10,48 +10,54 @@ import {Header} from "./components/Header/Header";
 import {useDebounce} from "./hooks/useDebounce";
 
 const Home = () => {
-  const [url, setUrl] = useState<string>("");
-  const debouncedValue = useDebounce<string>(url, url === "" ? 0 : 2000);
+    const [url, setUrl] = useState<string>("");
+    const debouncedValue = useDebounce<string>(url, url === "" ? 0 : 2000);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setUrl(event.target.value);
-  };
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setUrl(event.target.value);
+    };
 
-  return (
-      <div>
-        <Header/>
+    useEffect(() => {
+        // console.log(process.env.NODE_ENV.toString()) // development
+        const level: string | undefined = process.env.NEXT_PUBLIC_PINO_LOG_LEVEL
+        console.log(level?.toString()) // development
+    }, []);
+
+    return (
         <div>
-          <h1 className="title">
-            <Link
-                data-cy='link'
-                href={"/"}
-                onClick={() => setUrl("")}>
-              Webpage Viewer
-            </Link>
-          </h1>
-          <div>
-            <input
-                data-cy="url-input"
-                className="h1"
-                type="text"
-                value={url}
-                onChange={handleChange}
-                placeholder="Enter URL"
-            />
-          </div>
-          <div>
-            {url && (
-                <FetchWebsite
-                    data-cy="fetch-website"
-                    url={debouncedValue !== "" ? debouncedValue : url}
-                />
-            )}
-          </div>
+            <Header/>
+            <div>
+                <h1 className="title">
+                    <Link
+                        data-cy='link'
+                        href={"/"}
+                        onClick={() => setUrl("")}>
+                        Webpage Viewer
+                    </Link>
+                </h1>
+                <div>
+                    <input
+                        data-cy="url-input"
+                        className="h1"
+                        type="text"
+                        value={url}
+                        onChange={handleChange}
+                        placeholder="Enter URL"
+                    />
+                </div>
+                <div>
+                    {url && (
+                        <FetchWebsite
+                            data-cy="fetch-website"
+                            url={debouncedValue !== "" ? debouncedValue : url}
+                        />
+                    )}
+                </div>
+            </div>
+            <CookieBanner/>
+            <Feedback/>
         </div>
-        <CookieBanner/>
-        <Feedback/>
-      </div>
-  );
+    );
 };
 
 export default Home;
