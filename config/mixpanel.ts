5d978fcde445ced16dfb7847d983fab0c97a87a0
mohamedhassan <mohamedhassankhider@gmail.com>
2025-04-03 01:30:10 +0400

    fix github build node version

--- config/mixpanel.ts
+++ config/mixpanel.ts
@@ -1,11 +1,11 @@
 import mixpanel from 'mixpanel-browser';
 
-if (!process.env.NEXT_PUBLIC_MIXPANEL_TOKEN) {
-	throw new Error('MIXPANEL_TOKEN is not defined');
-}
+// if (!process.env.NEXT_PUBLIC_MIXPANEL_TOKEN) {
+// 	throw new Error('MIXPANEL_TOKEN is not defined');
+// }
 
 mixpanel.init(
-	process.env.NEXT_PUBLIC_MIXPANEL_TOKEN,
+	process.env.NEXT_PUBLIC_MIXPANEL_TOKEN!,
 	{
 		debug: false,
 		track_pageview: true,
